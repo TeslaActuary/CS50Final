6e4cebaa52cf5e4bfea516f05eb15ef468a8f79c
TeslaActuary <chris.allison28@yahoo.com>
2021-08-18 12:29:22 -0400

    CS50_v0
    Initial version added to Github

---
+++ Reptiles/ColdBlooded/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ColdbloodedConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ColdBlooded'
---
+++ Reptiles/ColdBlooded/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class User(AbstractUser):
+    pass
+
+class Snake(models.Model):
+
+    name = models.CharField(max_length=128, default='')
+    sciname = models.CharField(max_length=128, default='')
+    description = models.TextField(default ='')
+    picture = models.ImageField(blank=True)
+    range = models.CharField(max_length=128,null=True)
+    rangepic = models.URLField()
+    funfacts = models.TextField(null=True, blank=True)
+    is_venomous = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.name}: {self.sciname}"
+
+class Trivia(models.Model):
+    question = models.ImageField()
+    choice1 = models.CharField(max_length=200,null=True)
+    choice2 = models.CharField(max_length=200,null=True)
+    choice3 = models.CharField(max_length=200,null=True)
+    choice4 = models.CharField(max_length=200,null=True)
+    answer = models.CharField(max_length=200,null=True)
+    
+    def __str__(self):
+        return self.question
