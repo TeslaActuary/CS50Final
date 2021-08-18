26acd0cb2466ff3a733153255e4b978209818464
TeslaActuary <chris.allison28@yahoo.com>
2021-08-18 15:03:45 -0400

    version 1
    Set up forms and html to split work easily

--- Reptiles/ColdBlooded/urls.py
+++ Reptiles/ColdBlooded/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+    path('trivia', views.trivia, name='trivia'),
+    path('newtrivia', views.newtrivia, name='newtrivia'),
 ]
--- Reptiles/Reptiles/urls.py
+++ Reptiles/Reptiles/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ColdBlooded/', include('ColdBlooded.urls'))
+    path('', include('ColdBlooded.urls'))
 ]
