645358a93f5ca221c96ff0a1f815e2de2062bf9a
TeslaActuary <55221213+TeslaActuary@users.noreply.github.com>
2021-09-05 14:48:15 -0400

    Add files via upload

--- Reptiles/ColdBlooded/utils.py
+++ Reptiles/ColdBlooded/utils.py
@@ -6,6 +6,6 @@ import random
 def quiz_list(count):
     trivia_id_list = Trivia.objects.values_list('id', flat=True)
     random_trivia_id_list = random.sample(list(trivia_id_list), min(len(trivia_id_list),count))
-    trivia = Trivia.objects.filter(id__in=random_trivia_id_list)
+    trivia = Trivia.objects.filter(id__in=random_trivia_id_list).order_by('choice2')
 
     return trivia
